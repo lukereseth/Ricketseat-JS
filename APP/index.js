b155111cbe8830b714f3e3efd6f685767f4841cd
lukereseth <lucas.santos1914@etec.sp.gov.br>
2024-09-12 01:33:25 -0300

    fundamentos 001

---
+++ APP/index.js
@@ -0,0 +1,25 @@
+/*   const mensagem = "Olá eu"
+
+ {
+  const mensagem = "Olá, mundo!"
+  console.log(mensagem)
+ }
+
+ console.log(mensagem); */
+
+// arrays, objetos
+let meta = {
+  value: "ler um livro por mês",
+  checked: false,
+  log: (info) => {
+    console.log(info);
+  },
+};
+meta.value = "não é mais ler um livro";
+console.log(meta.value);
+
+// arrow function
+const criarMeta = () => {};
+
+// function
+// function criarMeta() {}
